1de1dac1069019df12e9d4739e55ea02f382b880
Crozzell05 <cdrozzell1@buffs.wtamu.edu>
2025-08-30 13:11:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Given Values
+        double X = 2.5;
+        double Y = 3.3;
+
+        //Equation: Z = 4*X*X + 3*Y
+        double Z = 4 * X * X + 3 * Y;
+
+        //Print result using formatted string
+        Console.WriteLine($"For X = {X} and Y = {Y}");
+        Console.WriteLine($"The value of Z is {Z:F2}");
+    }
+       
+}
