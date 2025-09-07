62eec88c75407a543b9fb57898d4065f9df12a92
Crozzell05 <cdrozzell1@buffs.wtamu.edu>
2025-09-06 20:01:56 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,87 @@
-﻿namespace HelloWorld;
+﻿namespace Week2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //Given Values
-        double X = 2.5;
-        double Y = 3.3;
+        // =======================
+        // Q1: Letter Grade to GPA
+        // =======================
+        Console.WriteLine("Q1: Enter a letter grade (A, B, C, D, E, F: )");
+        string grade = Console.ReadLine();
 
-        //Equation: Z = 4*X*X + 3*Y
-        double Z = 4 * X * X + 3 * Y;
+        switch (grade.ToUpper())
+        {
+            case "A":
+                Console.WriteLine("GPA Points: 4");
+                break;
+            case "B":
+                Console.WriteLine("GPA Points: 3");
+                break;
+            case "C":
+                Console.WriteLine("GPA Points: 2");
+                break;
+            case "D":
+                Console.WriteLine("GPA Points: 1");
+                break;
+            case "F":
+                Console.WriteLine("GPA Points: 0");
+                break;
+            default:
+                Console.WriteLine("Wrong Letter Grade!");
+                break;
+        }
 
-        //Print result using formatted string
-        Console.WriteLine($"For X = {X} and Y = {Y}");
-        Console.WriteLine($"The value of Z is {Z:F2}");
+        Console.WriteLine(); //break line for readability
+
+        //========================
+        //Q2: Find Smallest Number
+        //========================
+        Console.WriteLine("Q2: Enter three numbers to find the smallest value");
+
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        int num3 = Convert.ToInt16(Console.ReadLine());
+
+        int smallest;
+
+        if (num1 < num3)
+        {
+            if (num1 < num3)
+                smallest = num1;
+            else
+                smallest = num3;
+        }
+        else
+        {
+            if (num2 < num3)
+                smallest = num2;
+            else
+                smallest = num3;
+        }
+
+        Console.WriteLine("The smallest value is: " + smallest);
+
+        Console.WriteLine(); //blank line for readability
+
+        //=========================
+        // Bonus: Leap Year Checker
+        //=========================
+        Console.WriteLine("Bonus: Enter a year to check if it is a leap year.");
+        int year = Convert.ToInt16(Console.ReadLine());
+
+         if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            {
+                Console.WriteLine(year + " is a Leap Year.");
+            }
+            else
+            {
+                Console.WriteLine(year + " is NOT a Leap Year.");
+            }
     }
-       
 }
