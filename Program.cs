1b945beb7180980d8bea470dd7bc80d5c9de88f3
Crozzell05 <cdrozzell1@buffs.wtamu.edu>
2025-09-10 10:56:38 -0500

    Upload Homework3

--- Program.cs
+++ Program.cs
@@ -1,87 +1,80 @@
-﻿namespace Week2;
+﻿namespace Homework3;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // =======================
-        // Q1: Letter Grade to GPA
-        // =======================
-        Console.WriteLine("Q1: Enter a letter grade (A, B, C, D, E, F: )");
-        string grade = Console.ReadLine();
+            // ==============================
+            // Q1: Prime Check
+            // ==============================
+            Console.WriteLine("Q1: Prime Number Check");
+            Console.Write("Enter a number N: ");
+            int n1 = int.Parse(Console.ReadLine());
+            bool isPrime = true;
 
-        switch (grade.ToUpper())
-        {
-            case "A":
-                Console.WriteLine("GPA Points: 4");
-                break;
-            case "B":
-                Console.WriteLine("GPA Points: 3");
-                break;
-            case "C":
-                Console.WriteLine("GPA Points: 2");
-                break;
-            case "D":
-                Console.WriteLine("GPA Points: 1");
-                break;
-            case "F":
-                Console.WriteLine("GPA Points: 0");
-                break;
-            default:
-                Console.WriteLine("Wrong Letter Grade!");
-                break;
-        }
-
-        Console.WriteLine(); //break line for readability
-
-        //========================
-        //Q2: Find Smallest Number
-        //========================
-        Console.WriteLine("Q2: Enter three numbers to find the smallest value");
-
-        Console.Write("Enter first number: ");
-        int num1 = Convert.ToInt16(Console.ReadLine());
-
-        Console.Write("Enter second number: ");
-        int num2 = Convert.ToInt16(Console.ReadLine());
-
-        Console.Write("Enter third number: ");
-        int num3 = Convert.ToInt16(Console.ReadLine());
-
-        int smallest;
-
-        if (num1 < num3)
-        {
-            if (num1 < num3)
-                smallest = num1;
-            else
-                smallest = num3;
-        }
-        else
-        {
-            if (num2 < num3)
-                smallest = num2;
-            else
-                smallest = num3;
-        }
-
-        Console.WriteLine("The smallest value is: " + smallest);
-
-        Console.WriteLine(); //blank line for readability
-
-        //=========================
-        // Bonus: Leap Year Checker
-        //=========================
-        Console.WriteLine("Bonus: Enter a year to check if it is a leap year.");
-        int year = Convert.ToInt16(Console.ReadLine());
-
-         if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            if (n1 <= 1)
             {
-                Console.WriteLine(year + " is a Leap Year.");
+                isPrime = false;
             }
             else
             {
-                Console.WriteLine(year + " is NOT a Leap Year.");
+                for (int i = 2; i <= n1 - 1; i++)
+                {
+                    if (n1 % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
             }
+
+            if (isPrime)
+                Console.WriteLine($"{n1} is prime");
+            else
+                Console.WriteLine($"{n1} is non-prime");
+
+            Console.WriteLine(); // blank line
+
+
+            // ==============================
+            // Q2: Square Pattern
+            // ==============================
+            Console.WriteLine("Q2: Square Pattern");
+            Console.Write("Enter an integer N: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n2; i++)
+            {
+                for (int j = 0; j < n2; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(); // blank line
+
+
+            // ==============================
+            // Q3: Diagonal Square Pattern
+            // ==============================
+            Console.WriteLine("Q3: Diagonal Square Pattern");
+            Console.Write("Enter an integer N: ");
+            int n3 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n3; i++)
+            {
+                for (int j = 0; j < n3; j++)
+                {
+                    if (i >= j) // diagonal condition
+                        Console.Write("* ");
+                    else
+                        Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+
+        }
     }
-}
+    
+
